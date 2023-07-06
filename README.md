6805eec295a8d2b951f51a5024f96ef947dc68e8
jmLascano <jm.lascano2@gmail.com>
2023-07-06 04:45:17 -0400

    working code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # gifSplitter
+
+[Still WIP]
+
+This is a program that takes gifs and splits them into their individual images. You can either have the split images saved individually, or as one full object.
+
+Intended for use on a device running Windows 10 OS and Python 3.10.11+
+
+Ensure that you have PIL installed.
+
+---
+
+> How do I use it?
+
+Run from the command line.
--- main.py
+++ main.py
@@ -1,3 +1,45 @@
 import sys
 import os
 from PIL import Image
+
+# Ensuring initial command line input is proper
+
+try:
+    target = sys.argv[1]
+    mode = sys.argv[2]
+except:
+    print("Not all args supplied")
+    print("Format: py .\main.py [path] [i/t]")
+    exit()
+
+if not (os.path.isdir(target) or os.path.isfile(target)):
+    print("The path is not a valid folder or file.")
+    exit()
+
+if not (mode == 'i' or mode == 't'):
+    print("The mode must be either i or t.")
+    # exit()
+
+# Processing
+
+gif = Image.open(target)
+frames = []
+
+try:
+    while True:
+        current = gif.copy()
+        frames.append(current)
+        gif.seek(gif.tell() + 1)
+except EOFError:
+    pass
+
+width, height = gif.size
+finalWidth = width * len(frames)
+toSave = Image.new("RGBA", (finalWidth, height))
+offset = 0
+
+for frame in frames:
+    toSave.paste(frame, (offset, 0))
+    offset += width
+
+toSave.save("./output.png")
